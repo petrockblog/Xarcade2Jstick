1aa32467e7830d0cbfb2d7edfba36ef4b2377119
petrockblog <contact@petrockblock.com>
2019-03-26 21:00:45 +0100

    Added new device identifier

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 /*  along with this program; if not, write to the Free Software             */
 /*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.               */
 /* ======================================================================== */
-/*                 Copyright (c) 2014, Florian Mueller                      */
+/*                 Copyright (c) 2014-2019, Florian Mueller                 */
 /* ======================================================================== */
 
 #include <stdio.h>
